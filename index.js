cb941e28f4b73eb57550232edc83228893a4e19e
Andrew Armenia <andrew@asquaredlabs.com>
2017-05-29 12:58:34 -0400

    Add support for "if-then" conditions

--- index.js
+++ index.js
@@ -8,41 +8,69 @@ const badPatterns = [
 	/_stlawrence_/,
 ];
 
+const ifThenRules = [
+	{ 
+		ifPattern: /_w?hockey_/, 
+		thenPattern: /_p\d+(_x\d+)\.(mp4|mxf)/,
+		message: "Hockey must end in _pN or _pN_xN " +
+		         "and be mp4 or mxf files."
+	}
+];
+
+// Each checkX function (checkFormat, checkBadPatterns, checkIfThen)
+// returns an array of problems it's found.
 function checkFormat(filename) {
 	// filenames must be e.g. 20170208_hockey_union
 	var pattern = /^(\d{8})_[a-z0-9_]+\./;
 	var m = filename.match(pattern);
 
 	if (!m) {
-		return "incorrect format";
+		return ["incorrect format"];
 	}
 
 	var datestr = m[1];
 	if (!moment(datestr, 'YYYYMMDD').isValid()) {
-		return "invalid date";
+		return ["invalid date"];
 	}
 
-	return null;
+	return [];
 }
 
 function checkBadPatterns(filename) {
+	var ret = [];
 	for (var i = 0; i < badPatterns.length; i++) {
-		if (filename.match(badPatterns[i])) {
-			return "matched known bad pattern";
+		var badPattern = badPatterns[i];
+		if (filename.match(badPattern)) {
+			ret.push("matched known bad pattern " + badPattern);
 		}
 	}
 
-	return null;
+	return ret;
+}
+
+function checkIfThen(filename) {
+	var ret = [];
+	for (var i = 0; i < ifThenRules.length; i++) {
+		var rule = ifThenRules[i];
+		if (
+			filename.match(rule.ifPattern)
+			&& !filename.match(rule.thenPattern)
+		) {
+			ret.push(rule.message);
+		}
+	}
+	return ret;
 }
 
 function checkFile(filename) {
 	var checkers = [checkFormat, checkBadPatterns];
 	var basename = path.basename(filename);
+
 	for (var i = 0; i < checkers.length; i++) {
 		var fn = checkers[i];
-		var result = fn(basename);
-		if (result) {
-			console.log("%s: %s", filename, result);
+		var results = fn(filename);
+		for (var j = 0; j < newResults.length; j++) {
+			console.log(results[j]);	
 		}
 	}
 }
